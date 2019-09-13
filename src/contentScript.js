3da4a637e992ae413cb78d7beba52a0edbfd0545
Tapio Heiskanen <tapio.heiskanen@palmu.fi>
2019-09-13 11:56:01 +0300

    Add slight delay between digitransit queries

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,14 +1,16 @@
 import { addTripDurationToCard } from './cardModifier';
+import { delay } from './async';
 
 document.addEventListener('DOMContentLoaded', function(event) {
   const mutationObserver = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
+    mutations.forEach(async mutation => {
       if (
         mutation.addedNodes &&
         mutation.addedNodes[0] &&
         mutation.addedNodes[0].classList &&
         mutation.addedNodes[0].classList[0] === 'cards__card'
       ) {
+        await delay(100);
         addTripDurationToCard(mutation.addedNodes[0]);
       }
     });
