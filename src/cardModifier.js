8ede6285776070af562763d709d1e00f80722cfd
Tapio Heiskanen <tapio.heiskanen@palmu.fi>
2019-09-13 16:44:48 +0300

    Initial caching functionality

--- src/cardModifier.js
+++ src/cardModifier.js
@@ -4,7 +4,9 @@ import {
   getTripDurations
 } from './digitransit';
 
-const ids = [];
+const CARDS_KEY = 'OIKOTIE_CARDS';
+
+const cards = JSON.parse(localStorage.getItem(CARDS_KEY)) || {};
 
 export const addTripDurationToCard = async card => {
   const durationDiv = document.createElement('div');
@@ -22,23 +24,17 @@ export const addTripDurationToCard = async card => {
     return false;
   }
 
-  const id = link.getAttribute('ng-href');
-  const identified = ids.includes(id);
-  if (identified) {
-    return false;
-  }
-
-  ids.push(id);
-
+  const id = link
+    .getAttribute('ng-href')
+    .split('/')
+    .pop();
   const street = card.querySelector('.ot-card__street').textContent;
   const city = card.querySelector('.ot-card__text--concat').textContent;
   const address = `${street} ${city}`;
-  const coordinates = await getCoordinatesForAddress(address);
 
-  const tripDurations = await getTripDurations(coordinates);
-  if (!tripDurations || tripDurations.length === 0) {
-    return false;
-  }
+  const details = cards[id] || {};
+  const coordinates = details.coordinates || (await getCoordinatesForAddress(address)); // prettier-ignore
+  const tripDurations = details.tripDurations || (await getTripDurations(coordinates)); // prettier-ignore
 
   const journeyPlannerLink = getJourneyPlannerLink(address, coordinates);
   const tripDurationFastest = Math.round(Math.min(...tripDurations) / 60);
@@ -51,4 +47,15 @@ export const addTripDurationToCard = async card => {
   card.querySelector(
     '.ot-card__duration'
   ).innerHTML = `${tripDurationText} <a href="${journeyPlannerLink}" target="_blank">(reittiopas.fi)</a>`;
+
+  cards[id] = {
+    id,
+    street,
+    city,
+    address,
+    coordinates,
+    tripDurations
+  };
+
+  localStorage.setItem(CARDS_KEY, JSON.stringify(cards));
 };
